36c4a16c24e0754e9cc33d9e93fc2edd131905f7
HeYinKazune <1254462962@qq.com>
2025-05-12 15:32:14 +0900

    Small Updates: VideoHandler

--- src/utils/VideoHandler.cpp
+++ src/utils/VideoHandler.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "VideoHandler.h"
+
+VideoHandler::VideoHandler(String fileUrl) {
+    VideoCapture cap(fileUrl);
+    if (!cap.isOpened()) {
+        cerr << "Cannot Open Target Video File(s)." << endl;
+    }
+    {
+        Mat frame;
+        while (cap.read(frame)) {
+            this->frames.push_back(frame);
+        }
+    }
+    this->num_frame = frames.size();
+    this->fps = cap.get(CAP_PROP_FPS);
+    this->width = cap.get(CAP_PROP_FRAME_WIDTH);
+    this->height = cap.get(CAP_PROP_FRAME_HEIGHT);
+}
+
+Mat VideoHandler::getFrame(int idx) const {
+
+    if (idx < 0 || idx >= num_frame) {
+        return Mat::zeros(height, width, CV_8UC3);
+    }
+    return frames[idx];
+
+}
--- src/utils/VideoHandler.h
+++ src/utils/VideoHandler.h
@@ -12,29 +12,9 @@ using namespace std;
 
 class VideoHandler {
 public:
-    VideoHandler(String fileUrl) {
-        VideoCapture cap(fileUrl);
-        if (!cap.isOpened()) {
-            cerr << "Cannot Open Target Video File(s)." << endl;
-        }
-        {
-            Mat frame;
-            while (cap.read(frame)) {
-                this->frames.push_back(frame);
-            }
-        }
-        this->num_frame = frames.size();
-        this->fps = cap.get(CAP_PROP_FPS);
-        this->width = cap.get(CAP_PROP_FRAME_WIDTH);
-        this->height = cap.get(CAP_PROP_FRAME_HEIGHT);
-    }
+    VideoHandler::VideoHandler(String fileUrl);
 
-    Mat getFrame(int idx) const {
-        if (idx < 0 || idx >= num_frame) {
-            return Mat::zeros(height, width, CV_8UC3);
-        }
-        return frames[idx];
-    }
+    Mat getFrame(int idx) const;
 
 private:
     vector<Mat> frames;
